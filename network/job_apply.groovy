bcc35e8a07f3328d818ffd0769b1d4cd58544360
Piotr Majewski <majewski.piotr.511@gmail.com>
2025-03-12 12:15:50 +0100

    Modified credentials syntax again

--- network/job_apply.groovy
+++ network/job_apply.groovy
@@ -12,39 +12,41 @@ pipelineJob('network/apply') {
                             }
                         }
                         stage('Terraform Init') {
-                            steps withAWS(credentials: 'aws-terraform') {
-                                // Change into the network directory and initialize Terraform
-                                dir('network') {
-                                    sh 'terraform init'
+                            steps {
+                                withAWS(credentials: 'aws-terraform') {
+                                    dir('network') {
+                                        sh 'terraform init'
+                                    }
                                 }
                             }
                         }
                         stage('Terraform Plan') {
-                            steps withAWS(credentials: 'aws-terraform') {
-                                dir('network') {
-                                    sh 'terraform plan -var-file="../global.tfvars" -out=tfplan.out'
-                                    archiveArtifacts artifacts: 'tfplan.out', fingerprint: true
+                            steps {
+                                withAWS(credentials: 'aws-terraform') {
+                                    dir('network') {
+                                        sh 'terraform plan -var-file="../global.tfvars" -out=tfplan.out'
+                                        archiveArtifacts artifacts: 'tfplan.out', fingerprint: true
+                                    }
                                 }
                             }
                         }
                         stage('Manual Approval') {
                             steps {
-                                // Wait for manual approval before applying the plan
                                 input message: 'Review the Terraform plan output. Approve to proceed with apply?', ok: 'Apply'
                             }
                         }
                         stage('Terraform Apply') {
-                            steps withAWS(credentials: 'aws-terraform') {
-                                dir('network') {
-                                    // Apply the Terraform plan with the global variable file
-                                    sh 'terraform apply -var-file="../global.tfvars" tfplan.out'
+                            steps {
+                                withAWS(credentials: 'aws-terraform') {
+                                    dir('network') {
+                                        sh 'terraform apply -var-file="../global.tfvars" tfplan.out'
+                                    }
                                 }
                             }
                         }
                     }
                 }
             """)
-            sandbox()
         }
     }
 }
--- network/job_destroy.groovy
+++ network/job_destroy.groovy
@@ -12,17 +12,21 @@ pipelineJob('network/destroy') {
                             }
                         }
                         stage('Terraform Init') {
-                            steps withAWS(credentials: 'aws-terraform') {
-                                dir('network') {
-                                    sh 'terraform init'
+                            steps {
+                                withAWS(credentials: 'aws-terraform') {
+                                    dir('network') {
+                                        sh 'terraform init'
+                                    }
                                 }
                             }
                         }
                         stage('Terraform Plan Destroy') {
-                            steps withAWS(credentials: 'aws-terraform') {
-                                dir('network') {
-                                    sh 'terraform plan -destroy -var-file="../global.tfvars" -out=tfplan.destroy.out'
-                                    archiveArtifacts artifacts: 'tfplan.destroy.out', fingerprint: true
+                            steps {
+                                withAWS(credentials: 'aws-terraform') {
+                                    dir('network') {
+                                        sh 'terraform plan -destroy -var-file="../global.tfvars" -out=tfplan.destroy.out'
+                                        archiveArtifacts artifacts: 'tfplan.destroy.out', fingerprint: true
+                                    }
                                 }
                             }
                         }
@@ -32,16 +36,17 @@ pipelineJob('network/destroy') {
                             }
                         }
                         stage('Terraform Destroy') {
-                            steps withAWS(credentials: 'aws-terraform') {
-                                dir('network') {
-                                    sh 'terraform apply tfplan.destroy.out'
+                            steps {
+                                withAWS(credentials: 'aws-terraform') {
+                                    dir('network') {
+                                        sh 'terraform apply tfplan.destroy.out'
+                                    }
                                 }
                             }
                         }
                     }
                 }
             """)
-            sandbox()
         }
     }
 }
