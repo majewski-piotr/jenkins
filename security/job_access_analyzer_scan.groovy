4a1f64d4f934c78a3f8fbfb6c657ecb8cd068c88
Piotr Majewski <majewski.piotr.511@gmail.com>
2025-03-13 15:49:03 +0100

    Move test logic out of dsl declaration

--- security/job_access_analyzer_scan.groovy
+++ security/job_access_analyzer_scan.groovy
@@ -2,29 +2,41 @@ pipelineJob('security/network-access-analyzer-tests') {
   description('Connectivity tests using AWS CLI with Terraform outputs, preparing data and running connectivity tests script. WARNING: This job provisions infra elements using AWS CLI; if failures occur, please investigate the infrastructure state manually.')
   definition {
     cps {
-      script('''
+      script("""
         pipeline {
           agent any
 
           stages {
+            stage('Checkout') {
+              steps {
+                // Checkout the Terraform repo that includes the Terraform code and the /scripts folder
+                git url: https://github.com/majewski-piotr/aws-infra.git, branch: 'main'
+              }
+            }
             stage('Warning') {
               steps {
                 echo "WARNING: This job will use a bash script to provision infrastructure elements using AWS CLI."
                 echo "In case of failure, please investigate the infrastructure state manually."
               }
             }
-            stage('Retrieve Terraform Outputs') {
+            stage('Terraform Init & Retrieve Outputs') {
               steps {
-                sh 'terraform output -json > tf_outputs.json'
-                archiveArtifacts artifacts: 'tf_outputs.json', fingerprint: true
+                withAWS(credentials: params.CREDENTIALS_ID) {
+                  dir(network) {
+                    sh 'terraform init -no-color'
+                    sh 'terraform output -json > tf_outputs.json'
+                  }
+                }
+                // Archive the Terraform outputs file
+                archiveArtifacts artifacts: "scripts/tf_outputs.json", fingerprint: true
               }
             }
             stage('Run Connectivity Tests Script') {
               steps {
-                withAWS(credentials: 'aws-terraform') {
-                  script {
-                    sh 'chmod +x ../scripts/aws_connectivity_tests.sh'
-                    sh '../scripts/aws_connectivity_tests.sh tf_outputs.json'
+                withAWS(credentials: params.CREDENTIALS_ID) {
+                  dir(scripts) {
+                    sh 'chmod +x aws_connectivity_tests.sh'
+                    sh 'scripts/aws_connectivity_tests.sh tf_outputs.json'
                   }
                 }
               }
@@ -37,7 +49,7 @@ pipelineJob('security/network-access-analyzer-tests') {
             }
           }
         }
-      ''')
+      """)
     }
   }
 }
