7691e4857896eaf9dc2c95ba146d9d1cddb770d4
Piotr Majewski <majewski.piotr.511@gmail.com>
2025-03-22 21:31:13 +0100

    Add destroy_all_infra job

--- destroy_all_infrastructure.groovy
+++ destroy_all_infrastructure.groovy
@@ -1,45 +1,45 @@
-pipelineJob('apply_all_infra') {
-    description('job that builds all infrastructure')
+pipelineJob('destroy_all_infra') {
+    description('job that destroys all infrastructure')
     definition {
         cps {
             script("""
                 pipeline {
                     agent any
                     stages {
-                        stage('Build network') {
-                            steps {
-                                build job: 'terraform-job',
-                                      parameters: [
-                                          string(name: 'GIT_URL', value: 'https://github.com/majewski-piotr/aws-infra.git'),
-                                          string(name: 'CREDENTIALS_ID', value: 'aws-terraform'),
-                                          string(name: 'WORKING_DIR', value: 'network'),
-                                          booleanParam(name: 'DESTROY', value: false),
-                                          booleanParam(name: 'AUTO_APPROVE', value: true)
-                                      ],
-                                      wait: true
-                            }
-                        }
-                        stage('Build security') {
-                            steps {
-                                build job: 'terraform-job',
-                                      parameters: [
-                                          string(name: 'GIT_URL', value: 'https://github.com/majewski-piotr/aws-infra.git'),
-                                          string(name: 'CREDENTIALS_ID', value: 'aws-terraform'),
-                                          string(name: 'WORKING_DIR', value: 'security'),
-                                          booleanParam(name: 'DESTROY', value: false),
-                                          booleanParam(name: 'AUTO_APPROVE', value: true)
-                                      ],
-                                      wait: true
-                            }
-                        }
-                        stage('Test network') {
+                        stage('Destroy network tests') {
                             steps {
                                 build job: 'terraform-job',
                                       parameters: [
                                           string(name: 'GIT_URL', value: 'https://github.com/majewski-piotr/aws-infra.git'),
                                           string(name: 'CREDENTIALS_ID', value: 'aws-terraform'),
                                           string(name: 'WORKING_DIR', value: 'access_analyzer'),
-                                          booleanParam(name: 'DESTROY', value: false),
+                                          booleanParam(name: 'DESTROY', value: true),
+                                          booleanParam(name: 'AUTO_APPROVE', value: true)
+                                      ],
+                                      wait: true
+                            }
+                        }
+                        stage('Destroy security') {
+                            steps {
+                                build job: 'terraform-job',
+                                      parameters: [
+                                          string(name: 'GIT_URL', value: 'https://github.com/majewski-piotr/aws-infra.git'),
+                                          string(name: 'CREDENTIALS_ID', value: 'aws-terraform'),
+                                          string(name: 'WORKING_DIR', value: 'security'),
+                                          booleanParam(name: 'DESTROY', value: true),
+                                          booleanParam(name: 'AUTO_APPROVE', value: true)
+                                      ],
+                                      wait: true
+                            }
+                        }
+                        stage('Destroy network') {
+                            steps {
+                                build job: 'terraform-job',
+                                      parameters: [
+                                          string(name: 'GIT_URL', value: 'https://github.com/majewski-piotr/aws-infra.git'),
+                                          string(name: 'CREDENTIALS_ID', value: 'aws-terraform'),
+                                          string(name: 'WORKING_DIR', value: 'network'),
+                                          booleanParam(name: 'DESTROY', value: true),
                                           booleanParam(name: 'AUTO_APPROVE', value: true)
                                       ],
                                       wait: true
